579993b5aa7da6d31f754142fcc8f20e802a8a99
vcrawford <vcrawford01@gmail.com>
2017-07-06 17:32:46 -0400

    Made plotting more flexible

--- PlotResults.sh
+++ PlotResults.sh
@@ -1,7 +1,8 @@
 # Plots all results files in the current directory
 # Assumes there are no other things that end with ".xml"
 
-python Plots/DataPlots.py $(echo *.xml) results.png 200
+#python Plots/DataPlots.py $(echo *.xml) results.png 200
 
-#python Plots/DataPlots.py ExperimentsData/Algorithms/1/results_1499294592.xml results.png 200
+python Plots/DataPlots.py Plots/config.xml ExperimentsData/Algorithms/1/results_1499294592.xml ExperimentsData/Algorithms/1/results_1499294534.xml ExperimentsData/Algorithms/1/results_1499294567.xml results.png
 
+#python Plots/DataPlots.py Plots/config.xml ExperimentsData/Algorithms/1/results_1499294592.xml results.png
--- Plots/DataPlots.py
+++ Plots/DataPlots.py
@@ -1,48 +1,88 @@
 # Generate plots from an XML results file
-# Run like "python DataPlots.py results1.xml results2.xml ... output.png 100"
+# Run like "python DataPlots.py config.xml results1.xml results2.xml ... output.png"
 
 import xml.etree.ElementTree as et
 import sys
 import matplotlib.pyplot as plt
 
+#config file
+config = sys.argv[1]
+
 # all the results files
-files = sys.argv[1:-2]
+files = sys.argv[2:-1]
 
-max_n = int(sys.argv[-1])
+#output file
+output = sys.argv[-1]
 
-# each exp, alg pair gets its own marker/color
-exp_types = ['ER', 'WS']
-alg_types = ['greedy0', 'exact']
+# read in config file
 
-colors = {'ER':'r', 'WS':'b'}
-markers = {'greedy0':'--', 'exact':'^'}
+exps = []
+alg_types = []
+colors = {}
+exp_names = {}
+alg_names = {}
+markers = {}
+
+tree = et.parse(config)
+root = tree.getroot()
+exps_xml = root.findall("experiments")[0]
+
+for exp_xml in exps_xml:
+
+   exps.append(exp_xml.text)
+
+   colors[exp_xml.text] = exp_xml.attrib["color"]
+
+   exp_names[exp_xml.text] = exp_xml.attrib["name"]
+
+alg_types_xml = root.findall("algorithm_types")[0]
+
+for alg_type_xml in alg_types_xml:
+
+   alg_types.append(alg_type_xml.text)
+
+   markers[alg_type_xml.text] = alg_type_xml.attrib["marker"]
+
+   alg_names[alg_type_xml.text] = alg_type_xml.attrib["name"]
+
+
+# What we will measure on the x and y axis
+x_axis = root.findall("x_axis")[0].text
+y_axis = root.findall("y_axis")[0].text
+
+# What value we will cap the x axis at
+x_max = float(root.findall("x_max")[0].text)
+
+# Now plot the data
+
 handles = []
 
 for f in files:
 
    tree = et.parse(f)
    root = tree.getroot()
-   exp_type = root.attrib["type"]
+   exp = root.attrib["id"]
 
    for alg in alg_types:
 
-      n = []
-      size = []
+      x = []
+      y = []
 
       runs = root.findall(alg)
 
       for run in runs:
  
-         single_n = int(run.findall('n')[0].text)
+         single_x = float(run.findall(x_axis)[0].text)
 
-         if single_n <= max_n:
+         if single_x <= x_max:
 
-            n.append(single_n)
-            size.append(int(run.findall('size')[0].text))
+            x.append(single_x)
+            y.append(float(run.findall(y_axis)[0].text))
 
-      plt.plot(n, size, colors[exp_type] + markers[alg], label=exp_type + alg)
-      handles.append(exp_type+alg)
+      plt.plot(x, y, colors[exp] + markers[alg])
+ 
+      handles.append(exp_names[exp] + " " + alg_names[alg])
 
 plt.legend(handles, loc=2)
-plt.savefig(sys.argv[-2])
+plt.savefig(sys.argv[-1])
 
