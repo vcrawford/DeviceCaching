de6aef4b45cc2e25f6fcfa147238eeebedf88a55
vcrawford <vcrawford01@gmail.com>
2017-07-10 16:36:00 -0400

    Organized contact graph computation into separate function

---
+++ ContactGraph/ContactGraph.cpp
@@ -0,0 +1,85 @@
+using namespace std;
+
+// Takes in location data and computes contact graph from some number of the first days
+// stores data in contact_graph
+void contactGraphFromLocation(const int& n, const int& radius, const string& input,
+                              const string& output, const int& days,
+                              vector<vector <float>>& contact_graph) {
+
+   ifstream location_data;
+   location_data.open(input);
+
+   // time in simulation
+   int time = 0;
+
+   // point we are looking at the location of
+   int node = 0;
+
+   // a location
+   int x, y;
+
+   // locations of each device in a second
+   vector< pair<int, int> > locations (n, pair<int, int> (0, 0));
+
+   contact_graph = vector< vector<float>> (n, vector<float> (n, 0));
+
+   // now read in the location data
+  
+   string line;
+   stringstream line_strm;
+
+   while (getline(location_data, line)) {
+
+      // read in point
+      line_strm.clear();
+      line_strm.str(line);
+      line_strm >> x >> y;
+
+      locations[node] = make_pair(x, y);
+
+      if (node == (n - 1)) {
+
+         // we got to the end of a time step, update contact data
+         time++;
+
+         // break out if this was enough days
+         if (time > 60*60*24*days) break;
+
+         for (int i = 0; i < n; i++) {
+
+            // only check lower id nodes, since the graph is symmetric
+
+            for (int j = 0; j < i; j++) {
+
+               float distance = sqrt(pow(locations[i].first - locations[j].first, 2.0) +
+                                     pow(locations[i].second - locations[j].second, 2.0));
+
+               if (distance < radius) {
+
+                 contact_graph[i][j]++;
+                 contact_graph[j][i]++;
+               }
+            }
+         }
+
+         node = -1; // will be incremented to 0
+
+      } 
+
+      node++;
+   }
+
+   // normalize contact graph by total time
+
+   for (int i = 0; i < contact_graph.size(); i++) {
+
+      for (int j = 0; j < contact_graph[i].size(); j++) {
+
+         contact_graph[i][j] = contact_graph[i][j]/time;
+      }
+   }
+
+}
+
+
+
--- Slaw/ContactGraph.cpp
+++ Slaw/ContactGraph.cpp
@@ -9,91 +9,28 @@
 #include <list>
 #include <cmath>
 #include <map>
+#include "../ContactGraph/ContactGraph.cpp"
 
 using namespace std;
 
 int main(int agrc, char* argv[]) {
 
+   // input parameters
+
    int node_count = atoi(argv[1]);
    string location_data_file = argv[2];
-   int radius = atoi(argv[3]); // the radius that two devices are considered in contact
+   int radius = atoi(argv[3]);
    string contact_graph_file = argv[4];
+   int days = stoi(argv[5]);
 
-   ifstream location_data;
-   location_data.open(location_data_file);   
-   int time = 1; // The time in simulation we are (in seconds from the beginning)
-   int node = 0; // What point we are looking at the location x,y of
-   int x, y; // the location we read in
-   vector< pair<int, int> > locations; // The locations of each device
-   vector< vector <float> > contact_graph; // ith, jth entry is the time the ith device has been in contact with the
-                                           // jth device over the entire simulation
+   // generate contact graph
+
+   vector< vector <float> > contact_graph;
 
    cout << "Generating contact data for " << node_count << " devices ..." << endl;
 
-   // From the first second data, get locations, add blank maps to in_contact, and fill in contact_graph
-   // with 0s
-   string line;
-   stringstream line_strm;
-   while (getline(location_data, line)) {
-
-      // read in point
-      line_strm.clear();
-      line_strm.str(line);
-      line_strm >> x >> y;
-
-      // Add first location
-      locations.push_back(make_pair(x, y));
-
-      // Add row in contact_graph
-      contact_graph.push_back(vector<float> (node_count, 0.0));
- 
-      if (node == (node_count - 1)) {
-         // we have got to the end of the first second
-         break;
-      }
-
-      node++;
-   }
-
-   node = 0;
-   int time_steps = 0;
-   // Compute contact graph
-   // doesn't include the first second, but that's okay
-   while (getline(location_data, line)) {
-
-      // read in point
-      line_strm.clear();
-      line_strm.str(line);
-      line_strm >> x >> y;
-
-      locations[node] = make_pair(x, y);
-
-      if (node == (node_count - 1)) {
-         // we got to the end of a time step, update contact data
-         time_steps++;
-
-         for (int i = 0; i < node_count; i++) {
-            //cout << "Check contact for " << i << endl;
-            // only check lower id nodes, since the graph is symmetric
-            for (int j = 0; j < i; j++) {
-
-               float distance = sqrt(pow(locations[i].first - locations[j].first, 2.0) +
-                                     pow(locations[i].second - locations[j].second, 2.0));
-
-               if (distance < radius) {
-                 contact_graph[i][j]++;
-                 contact_graph[j][i]++;
-               }
-            }
-         }
-
-         node = -1; // will be incremented to 0
-      }       
-
-      node++;
-   }
-
-   location_data.close();
+   contactGraphFromLocation(node_count, radius, location_data_file, contact_graph_file,
+                            days, contact_graph);
 
    // write contact graph to file
    ofstream contact_data_file;
@@ -103,7 +40,7 @@ int main(int agrc, char* argv[]) {
 
       for (int j = 0; j < contact_graph[i].size(); j++) {
 
-         contact_data_file << contact_graph[i][j]/time_steps << "  ";
+         contact_data_file << contact_graph[i][j] << "  ";
       }
 
       contact_data_file << endl;
